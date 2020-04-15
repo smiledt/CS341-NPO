8e49d07d8c35ef25ff8b60db6e3e7bea3a364ce8
smiledt <dereksmilees@gmail.com>
2020-04-14 23:52:26 -0500

    Continuing refactor

---
+++ book_keeping/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+# This is the main index page of the site
+# The first page that is loaded when the site is loaded
+
+app_name = 'book_keeping'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('events/', views.events, name='events'),
+    path('new_event/', views.new_event, name='new_event'),
+    path('add_vol/', views.add_vol, name='add_vol'),
+]
---
+++ book_keeping/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from .models import Event
+from .forms import EventForm
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+
+def index(request):
+    # Home Page for Book Keeping Soft
+    return render(request, 'book_keeping/index.html')
+
+
+def events(request):
+    # Show Events
+    events = Event.objects.order_by('date_of_event')
+    events_dict = {'events': events}
+    return render(request, 'book_keeping/events.html', events_dict)
+
+
+@login_required
+def new_event(request):
+    # Add a new event
+    if request.method != 'POST':
+        form = EventForm()
+    else:
+        form = EventForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_keeping:events')
+
+    context = {'form': form}
+    return render(request, 'book_keeping/new_event.html', context)
+
+
+@login_required
+def add_vol(request):
+    if request.method != 'POST':
+        temp = request.GET['submit']
+        # print(temp)
+        temp_obj = Event.objects.get(name__startswith=temp)
+        # print(temp_obj.num_volunteers)
+        # We have enough volunteers, do nothing
+        if (temp_obj.num_volunteers_total - temp_obj.num_volunteers) <= 0:
+            return redirect('book_keeping:events')
+        else:
+            temp_obj.num_volunteers += 1
+            temp_obj.save()
+            return redirect('book_keeping:events')
