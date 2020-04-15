e64279b6568a27dc34ce99d217102ce601bf55ab
smiledt <dereksmilees@gmail.com>
2020-04-14 23:52:11 -0500

    Major refactor

--- bookkeeping/book_keepings/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BookKeepingsConfig(AppConfig):
-    name = 'book_keepings'
--- bookkeeping/book_keepings/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Event
-
-class EventForm(forms.ModelForm):
-	class Meta:
-		model = Event
-		fields = ['name', 'date_of_event', 'start_time', 'end_time', 'location', 'description', 'num_volunteers_needed']
