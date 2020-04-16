7366d2220003d7a58812a25d0aed62c38bdb6e28
smiledt <dereksmilees@gmail.com>
2020-04-15 23:04:13 -0500

    Much of it works, still need to create the many-to-many volunteer table

--- book_keeping/views.py
+++ book_keeping/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Event
-from .forms import EventForm
+from .forms import EventForm, DonationForm
 from django.contrib.auth.decorators import login_required
+from users.models import UserAccountInfo
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -14,7 +16,14 @@ def index(request):
 def events(request):
     """ Show Events """
     events = Event.objects.order_by('date_of_event')
-    events_dict = {'events': events}
+    events_dict = {'events': events, 'is_donor': False}
+    this_user = request.user
+    if str(this_user) != 'AnonymousUser':  # Somebody is logged in
+        current_donor = this_user.useraccountinfo.is_donor()
+        if current_donor:
+            events_dict = {'events': events, 'is_donor': True}
+            print('IT WORKED!')
+
     return render(request, 'book_keeping/events.html', events_dict)
 
 
@@ -38,7 +47,7 @@ def add_vol(request):
     """ Add a volunteer to an event """
     # This method needs to be tweaked and changed by Derek
     if request.method != 'POST':
-        temp = request.GET['submit']
+        temp = request.GET['volunteer']
         # print(temp)
         temp_obj = Event.objects.get(name__startswith=temp)
         # print(temp_obj.num_volunteers)
@@ -49,3 +58,29 @@ def add_vol(request):
             temp_obj.num_volunteers += 1
             temp_obj.save()
             return redirect('book_keeping:events')
+
+
+@login_required
+def donate(request):
+    """ Create a donation """
+    if request.method != 'POST':
+        form = DonationForm()
+        event_name = request.GET['donate']
+        context = {'form': form, 'event_name': event_name}
+
+        return render(request, 'book_keeping/donate.html', context)
+    else:
+        temp_event = Event.objects.get(name__startswith=request.POST['donate'])
+        form = DonationForm(data=request.POST)
+        form.event_name = temp_event  # Do we need this?
+        # form.fields.update([('event_name', temp_event)])
+        # form = DonationForm(data=request.POST, initial={'event_name': temp_event})
+        print(temp_event)
+        if form.is_valid():
+            donation = form.save(commit=False)
+            donation.event_name = temp_event
+            donation.save()
+            return redirect('book_keeping:events')
+
+    context = {'form': form}
+    return render(request, 'book_keeping/donate.html', context)
